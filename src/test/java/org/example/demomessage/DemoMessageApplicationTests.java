6ba729d8cfc216ecdec0e905014ea904e2d219fd
SebastianMunt <semu0001@stud.kea.dk>
2025-10-01 08:54:07 +0200

    Message-App

---
+++ src/test/java/org/example/demomessage/DemoMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demomessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
